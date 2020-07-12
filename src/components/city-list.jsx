7ae779edcb95644324a4942e32fd8d425f826f44
amaury ropert <amauryropert@gmail.com>
2020-07-12 13:46:46 +0200

    created two containers

---
+++ src/components/city-list.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class CityList extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return null;
+  }
+}
+
+export default CityList;
---
+++ src/containers/city.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class City extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return null;
+  }
+}
+
+export default City;
